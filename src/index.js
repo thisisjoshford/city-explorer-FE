c4dbd4436a59455e6cadd1d7e51e9a993aff5c0d
Josh Ford <thisisjoshford@gmail.com>
2020-05-13 21:32:24 -0700

    redux store

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App/App';
+import { Provider } from 'react-redux';
+import reduxStore from './reduxStore';
 
 render(
-  <App />,
+  <Provider store={reduxStore}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
