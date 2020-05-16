1e24dd7abe226c7c12d4c17f9fb28fc2c59368c6
Josh Ford <thisisjoshford@gmail.com>
2020-05-15 17:03:50 -0700

    trail link updated

--- src/components/Trails/Trails.jsx
+++ src/components/Trails/Trails.jsx
@@ -9,11 +9,10 @@ const Trails = () => {
   const trailListings = trails.map((trail, i) => (
     <tr key={i}>
       <td>{trail.length} mi</td>
-      <td>{trail.name}</td>
+      <td><a href={trail.trail_url} target="_blank" rel="noreferrer">{trail.name}</a></td>
       <td>{trail.location}</td>
       <td>{trail.conditions}</td>
       <td>{trail.condition_date}</td>
-      <td><a href={trail.trail_url} target="_blank" rel="noreferrer">LINK</a></td>
     </tr>
   ));
   return (
@@ -25,7 +24,6 @@ const Trails = () => {
           <th>Location</th>
           <th>Conditions</th>
           <th>Time Stamp</th>
-          <th>LINK</th>
         </tr>
         { trailListings }
       </table>
